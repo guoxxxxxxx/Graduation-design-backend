027194f30b2ff38a704e5eaffdc4684bf6c5c394
guo_x <guo_x0315@163.com>
2022-03-29 09:35:54 +0800

    init

---
+++ other_files/Database_File/插入测试数据.sql
@@ -0,0 +1,15 @@
+INSERT INTO USER(NAME, sex, birthday, phone, faculty, grade, major, email, wechat, qq)
+VALUES('李亚斌', '男', '1999-09-09', '13343466992', '信息学院', '2018', '软件工程', 
+	'lyb@163.com', 'lyb_wechat', '88888888');
+INSERT INTO USER(NAME, sex, birthday, phone, faculty, grade, major, email, wechat, qq)
+VALUES('李子炫', '男', '2000-09-09', '13333333333', '信息学院', '2018', '软件工程', 
+	'lzx@163.com', 'lzx_wechat', '88888888');
+INSERT INTO USER(NAME, sex, birthday, phone, faculty, grade, major, email, wechat, qq)
+VALUES('张思恺', '男', '2000-09-19', '13212366992', '信息学院', '2018', '软件工程', 
+	'zsk@163.com', 'zsk_wechat', '88888888');
+INSERT INTO USER(NAME, sex, birthday, phone, faculty, grade, major, email, wechat, qq)
+VALUES('谭帅华', '男', '1999-09-19', '13456766992', '信息学院', '2018', '软件工程', 
+	'tsh@163.com', 'tsh_wechat', '88888888');
+INSERT INTO USER(NAME, sex, birthday, phone, faculty, grade, major, email, wechat, qq)
+VALUES('郭迅', '男', '2000-03-15', '15533466992', '信息学院', '2018', '软件工程', 
+	'gx@163.com', 'gx_wechat', '88888888');
