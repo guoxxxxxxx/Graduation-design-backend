a96fc0c98f126c262fe9e6e43ff99aa4ede4a604
guo_x <guo_x0315@163.com>
2022-03-29 22:00:42 +0800

    完成了，post请求的发送

---
+++ src/main/java/com/hebust/entity/user/User.java
@@ -0,0 +1,79 @@
+package com.hebust.entity.user;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * user
+ * @author 
+ */
+@Data
+public class User implements Serializable {
+    /**
+     * id
+     */
+    private Integer uid;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 性别, 仅有(男,女,保密)三个值, 默认为保密
+     */
+    private String sex;
+
+    /**
+     * 生日 格式为yyyy-MM-dd
+     */
+    private Date birthday;
+
+    /**
+     * 电话号码, 固定长度为11位
+     */
+    private String phone;
+
+    /**
+     * 所属院系
+     */
+    private String faculty;
+
+    /**
+     * 所属年级, 例如: 2018
+     */
+    private String grade;
+
+    /**
+     * 所属专业
+     */
+    private String major;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 微信号
+     */
+    private String wechat;
+
+    /**
+     * qq号
+     */
+    private String qq;
+
+    /**
+     * 是否删除, 1: 删除; 0: 未删除; 默认为0
+     */
+    private Integer isDelete;
+
+    /**
+     * 用户登录密码
+     */
+    private String password;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/hebust/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.hebust.mapper;
+
+import com.hebust.entity.user.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer uid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer uid);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    String selectPasswordByEmail(String email);
+}
