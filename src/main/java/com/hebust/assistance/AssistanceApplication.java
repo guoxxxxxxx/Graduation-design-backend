9873a67490a107e7f617d7ae92f8b7a7a4a3483f
guo_x <guo_x0315@163.com>
2022-03-29 09:27:20 +0800

    init

---
+++ src/main/java/com/hebust/assistance/AssistanceApplication.java
@@ -0,0 +1,13 @@
+package com.hebust.assistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssistanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hebust/assistance/AssistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hebust.assistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
