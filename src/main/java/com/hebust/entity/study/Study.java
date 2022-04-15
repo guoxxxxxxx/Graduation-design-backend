0d07bd78af22531f95ae327b8b0b166dc39c16ad
guo_x <guo_x0315@163.com>
2022-04-15 10:59:14 +0800

    完成了学习区功能的部分开发

---
+++ src/main/java/com/hebust/entity/study/Study.java
@@ -0,0 +1,61 @@
+package com.hebust.entity.study;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.hebust.entity.user.User;
+import lombok.Data;
+
+/**
+ * study
+ * @author 
+ */
+@Data
+public class Study implements Serializable {
+    /**
+     * 主键
+     */
+    private Integer sid;
+
+    /**
+     * 发帖用户, 外：user(uid)
+     */
+    private Integer uid;
+
+    /**
+     * 发布该信息的用户详细信息
+     */
+    private User pubUser;
+
+    /**
+     * 类别,可选项为: 数学, 物理, 英语, 其他
+     */
+    private String category;
+
+    /**
+     * 题目
+     */
+    private String title;
+
+    /**
+     * 详细内容
+     */
+    private String details;
+
+    /**
+     * 提交时间
+     */
+    private Date pubdate;
+
+    /**
+     * 是否已解决, 1:已解决 0:未解决 默认为0
+     */
+    private Integer isAchieve;
+
+    /**
+     * 是否删除, 1: 删除; 0: 未删除; 默认为0
+     */
+    private Integer isDelete;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/hebust/mapper/StudyMapper.java
@@ -0,0 +1,29 @@
+package com.hebust.mapper;
+
+import com.hebust.entity.study.Study;
+
+import java.util.List;
+
+public interface StudyMapper {
+    int deleteByPrimaryKey(Integer sid);
+
+    int insert(Study record);
+
+    int insertSelective(Study record);
+
+    Study selectByPrimaryKey(Integer sid);
+
+    int updateByPrimaryKeySelective(Study record);
+
+    int updateByPrimaryKey(Study record);
+
+    /**
+     * 查询所有学习项目 并查询用户信息
+     */
+    List<Study> selectAll();
+
+    /**
+     * 查询所有学习项目数量
+     */
+    int selectAllItemCount();
+}
